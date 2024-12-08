487c1e1b5fb42d6efd30cf6cf1006038b09d8aa5
87ehnosso <114113142+danilolopes87@users.noreply.github.com>
2024-12-08 19:34:33 -0300

    feat: add MongoGetUsersRepository

--- src/controllers/get-users/get-users.ts
+++ src/controllers/get-users/get-users.ts
@@ -1,6 +1,6 @@
 import { IGetUsersController, IGetUsersRepository } from "./protocols";
 
-export class GetUsersControllers implements IGetUsersController {
+export class GetUsersController implements IGetUsersController {
   constructor(private readonly getUsersRepository: IGetUsersRepository) {}
   async handle() {
     //validar requisição
