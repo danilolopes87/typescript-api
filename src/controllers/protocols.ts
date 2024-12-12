5eb4bd69d8a5accaf72b16060b89f58a5fa1ea7c
87ehnosso <114113142+danilolopes87@users.noreply.github.com>
2024-12-12 13:29:05 -0300

    feat: add CreateUserController with validations

--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -1,4 +1,10 @@
 export interface HttpResponse<T> {
-  statusCode: Number;
+  statusCode: number;
   body: T | string;
+}
+
+export interface HttpRequest<B> {
+  params?: any;
+  headers?: any;
+  body?: B;
 }
