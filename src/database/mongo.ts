547fd975446006d0b82b6f26086b799c3d0617b4
87ehnosso <114113142+danilolopes87@users.noreply.github.com>
2024-12-17 18:01:32 -0300

    Refatorar a conexão com o MongoDB: adicionar tratamento de erro e melhoria na conexão

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -3,25 +3,43 @@ import { MongoUser } from "../repositories/mongo-protocols";
 import { User } from "../models/user";
 
 export const MongoClient = {
-  client: undefined as unknown as Mongo,
-  db: undefined as unknown as Db,
+  client: null as unknown as Mongo,
+  db: null as unknown as Db,
 
   async connect(): Promise<void> {
-    const url = process.env.MONGODB_URL || "localhost:27017";
-    const username = process.env.MONGO_USERNAME;
-    const password = process.env.MONGO_PASSWORD;
+    try {
+      const url = process.env.MONGODB_URL || "mongodb://localhost:27017";
+      const username = process.env.MONGO_USERNAME;
+      const password = process.env.MONGO_PASSWORD;
 
-    const client = new Mongo(url, { auth: { username, password } });
-    const db = client.db("users-db");
+      const client =
+        username && password
+          ? new Mongo(url, { auth: { username, password } })
+          : new Mongo(url);
 
-    this.client = client;
-    this.db = db;
+      await client.connect();
 
-    console.log("connected to mongodb!");
+      const db = client.db("users-db");
+
+      this.client = client;
+      this.db = db;
+
+      console.log("Connected to MongoDB!");
+    } catch (error) {
+      console.error("Failed to connect to MongoDB", error);
+      throw error; // Lança o erro para que possa ser tratado no nível superior
+    }
   },
+
   mapUser(mongoUser: MongoUser): User {
     const { _id, ...rest } = mongoUser;
-
     return { id: _id.toHexString(), ...rest };
+  },
+
+  async disconnect(): Promise<void> {
+    if (this.client) {
+      await this.client.close();
+      console.log("Disconnected from MongoDB.");
+    }
   },
 };
